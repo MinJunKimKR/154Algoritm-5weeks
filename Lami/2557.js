852f0d2d9c911c5e324bed0b6ff3e3cab40495fd
Lami <87893624+HalamLee@users.noreply.github.com>
2022-01-16 23:23:39 +0900

    2557: Hello World

---
+++ Lami/2557.js
@@ -0,0 +1,13 @@
+/*********************************************************************
+ *Title : Hello World
+ *Number : <img src="https://d2gd6pc034wcta.cloudfront.net/tier/1.svg" class="solvedac-tier">&nbsp;2557번
+ *Author : sandoll99
+ *Description : Hello World!를 출력하시오.
+ *Input : 없음
+ *Output : Hello World!를 출력하시오.
+ *Start Time : 2022년 0월 16일 20시 47분 55초
+ *End Time : 2022년 0월 16일 시 분 초
+ *comments program : https://chrome.google.com/webstore/detail/boj-blog-post/ehhcjhfkaiohgflkjifmageahncijacd?hl=ko
+ *********************************************************************/
+
+console.log('Hello World!');
