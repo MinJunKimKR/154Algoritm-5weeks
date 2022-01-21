7e5a40832ecc182e6bf567a072c312ed77a6f252
kimmjieun <kimmjieun@gmail.com>
2022-01-21 22:31:50 +0900

    [Sorting] 11004번 K번째 수

---
+++ JE/1_21/boj_11004.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_11004 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        System.out.println(arr[k - 1]);
+
+    }
+}
