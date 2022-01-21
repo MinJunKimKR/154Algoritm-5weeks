7eec955a41983fd60968985329df337364fa6b10
kimmjieun <kimmjieun@gmail.com>
2022-01-21 22:30:12 +0900

    [Sorting] 11651번 좌표 정렬하기 2

---
+++ JE/1_21/boj_11651.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class boj_11651 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1])
+                    return o1[0] - o2[0];
+                else
+                    return o1[1] - o2[1];
+            }
+        });
+
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+        }
+        System.out.println(sb);
+    }
+}
