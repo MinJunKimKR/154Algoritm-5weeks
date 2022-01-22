68851626fd8d9d0af544561efa2592f594b4ccf3
kimmjieun <kimmjieun@gmail.com>
2022-01-22 23:05:33 +0900

    [Stack] 10799번 쇠막대기

---
+++ JE/1_22/boj_10799.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class boj_10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
+
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push('(');
+                continue;
+            }
+            if (input.charAt(i) == ')') {
+                stack.pop();
+                if (input.charAt(i - 1) == '(') {
+                    result += stack.size();
+                } else {
+                    result++;
+                }
+            }
+        }
+
+        System.out.println(result);
+    }
+}
