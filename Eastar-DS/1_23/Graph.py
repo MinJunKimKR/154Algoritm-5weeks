84ae52c660f7da03edc0aa24e98642388db351dd
Eastar-DS <dororo0104@naver.com>
2022-01-24 12:53:51 +0900

    0124

--- Eastar-DS/1_23/Graph.py
+++ Eastar-DS/1_23/Graph.py
@@ -63,9 +63,41 @@ print(' '.join([str(num) for num in out2]))
 
 
 
+#11724
+import sys,collections
+n,m = map(int,sys.stdin.readline().split())
+graph,visited,output = collections.defaultdict(list),[0]*n,0
+for _ in range(m):
+    u,v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+    
+def bfs(x):
+    queue = collections.deque([x])
+    while(queue):
+        num = queue.popleft()
+        for node in graph[num]:
+            if(visited[node-1] == 0):
+                queue.append(node)
+                visited[node-1] = 1    
+for i in range(n):
+    if(visited[i] == 0):
+        output += 1
+        bfs(i+1)
+print(output)
 
-
-
+#dfs
+sys.setrecursionlimit(10000)
+def dfs(x):
+    visited[x-1] = 1
+    for node in graph[x]:
+        if(visited[node-1] == 0):
+            dfs(node)
+for i in range(n):
+    if(visited[i] == 0):
+        output += 1
+        dfs(i+1)
+print(output)
 
 
 
