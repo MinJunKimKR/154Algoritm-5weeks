9deff02ae6e59b96c7c8095f964e756c63823e02
kimmjieun <kimmjieun@gmail.com>
2022-01-25 17:26:03 +0900

    [Strings] 2743번 단어 길이 재기

---
+++ JE/1_25/boj_2743.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class boj_2743 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(str.length());
+    }
+}
