1cb5c01d3ffa015b1f3a3b1c842246f3b951ef18
minjun.kim <rlaalswns321@naver.com>
2022-02-14 21:28:50 +0900

    feat: JH폴더 제거

--- JH/22.01.17/11022.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-n = int(input())
-for i in range(n):
-  try:
-    a, b = map(int, sys.stdin.readline().split())
-    print(f"Case #{i+1}: {a} + {b} = {a+b}")
-  except:
-    break
--- JH/22.01.19/2438.py
+++
@@ -1,4 +0,0 @@
-n = ["*"*x  for x in range(1, int(input())+1)] 
-print('\n'.join(n))
-
-# 그냥 바로 for 문 => print 로 출력해도 될 것 같다..
