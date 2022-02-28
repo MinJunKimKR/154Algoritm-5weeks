94f77ed58bb63be574d7cc8ef84aa40dcdd5edc7
kimmjieun <kimmjieun@gmail.com>
2022-02-28 18:42:05 +0900

    [Sorting] 11651번 좌표 정렬하기 2

--- JE/1_21/boj_11651.java
+++ JE/1_21/boj_11651.java
@@ -9,8 +9,8 @@ public class boj_11651 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(br.readLine());
+
         int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -29,8 +29,10 @@ public class boj_11651 {
         });
 
         for (int i = 0; i < n; i++) {
-            sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
         }
+
         System.out.println(sb);
+
     }
 }
