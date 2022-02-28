b3b11961c03dd1c5f23c94226f21a3a463f572cf
kimmjieun <kimmjieun@gmail.com>
2022-02-28 18:44:43 +0900

    [Sorting] 10825번 국영수

--- JE/1_21/boj_10825.java
+++ JE/1_21/boj_10825.java
@@ -9,7 +9,6 @@ public class boj_10825 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(br.readLine());
 
         String[][] arr = new String[n][4];
@@ -21,25 +20,24 @@ public class boj_10825 {
             arr[i][3] = st.nextToken();
         }
 
+
         Arrays.sort(arr, new Comparator<String[]>() {
+
             @Override
             public int compare(String[] o1, String[] o2) {
-                if (Integer.parseInt(o1[1]) == Integer.parseInt(o2[1])) {
-                    if (Integer.parseInt(o1[2]) == Integer.parseInt(o2[2])) {
-                        if (Integer.parseInt(o1[3]) == Integer.parseInt(o2[3])) {
-                            // 국영수 점수가 같다면 사전 오름차순
-                            return o1[0].compareTo(o2[0]);
+                if (Integer.parseInt(o1[1]) == Integer.parseInt(o2[1])) { // 국어 점수 같을 때
+                    if (Integer.parseInt(o1[2]) == Integer.parseInt(o2[2])) { // 영어 점수 같을 때
+                        if (Integer.parseInt(o1[3]) == Integer.parseInt(o2[3])) { // 수학 점수 같을 때
+                            return o1[0].compareTo(o2[0]); // 이름 사전순 증가
                         }
+                        return Integer.parseInt(o2[3]) - Integer.parseInt(o1[3]); // 수학 감소
 
-                        // 국어점수 같고 영어점수 같을 때, 수학점수는 내림차순
-                        return Integer.compare(Integer.parseInt(o2[3]), Integer.parseInt(o1[3]));
                     }
+                    return Integer.parseInt(o1[2]) - Integer.parseInt(o2[2]); // 영어 증가
 
-                    // 국어점수 같을 때, 영어점수는 오름차순
-                    return Integer.compare(Integer.parseInt(o1[2]), Integer.parseInt(o2[2]));
                 }
-                // 국어점수는 내림차순
-                return Integer.compare(Integer.parseInt(o2[1]), Integer.parseInt(o1[1]));
+                return Integer.parseInt(o2[1]) - Integer.parseInt(o1[1]); // 국어 감소
+
             }
         });
 
